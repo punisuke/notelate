06104a638c7d90cabd9028fad4c83eb9b0aed493
punisuke <20900274+punisuke@users.noreply.github.com>
2020-03-08 18:30:16 +0900

    initial version to release

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # notelate
+
 generate jupyter notebook/lab template via command line
+
+# Todo
+
+separate a space to store templates and download it only when a template is requested
--- notelate/scripts/app.py
+++ notelate/scripts/app.py
@@ -2,6 +2,7 @@ import requests
 import os
 import sys
 import glob
+import shutil
 
 
 class Notelate:
@@ -9,6 +10,7 @@ class Notelate:
         path = os.path.dirname(os.path.abspath(
             __file__)).replace('\\scripts', '')
         self.template_path = path + '\\templates\\'
+        print('template folder is [{}]'.format(self.template_path))
 
     def fetch_template(self, tempalte_name):
         base_url = ''
@@ -17,7 +19,8 @@ class Notelate:
         if local:
             temp_list = glob.glob(self.template_path+'*.ipynb')
         else:
-            temp_list = ['fa', 'b']
+            temp_list = glob.glob(self.template_path+'*.ipynb')
+
         for t in temp_list:
             print(t.split('\\')[-1].replace('.ipynb', ''))
 
@@ -27,21 +30,32 @@ class Notelate:
         else:
             return False
 
+    def copy_template(self, template_name):
+        if self.check_local(template_name):
+            template_name += '.ipynb'
+            old_path = self.template_path + template_name
+            new_path = os.getcwd() + '/' + template_name
+            shutil.copyfile(old_path, new_path)
+            print('"{}" is generated'.format(template_name))
+        else:
+            print('"{}" does not exist'.format(template_name))
+
 
 def main():
     nl = Notelate()
     if len(sys.argv) == 0:
         templat_name = 'basic'
+        nl.copy_template(templat_name)
 
     if sys.argv[1] == 'list':
-        print('printing template list in github')
+        print('printing template list in local')
         nl.print_list(local=False)
     elif sys.argv[1] == 'local':
         print('printing template list in local')
         nl.print_list(local=True)
     else:
         templat_name = sys.argv[1]
-        print(templat_name)
+        nl.copy_template(templat_name)
 
 
 if __name__ == '__main__':
